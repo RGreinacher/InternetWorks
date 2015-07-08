9502398db0ea59dcb71c782e793878ce0fbb8691
Robert Greinacher <robert@3antworten.de>
2015-07-08 11:56:05 +0200

    added daemon mode and readme text

--- InternetWorks.py
+++ InternetWorks.py
@@ -12,81 +12,112 @@ from PyQt4.QtCore import QThread, SIGNAL
 import requests
 import icons
 
+import argparse
+from daemonize import Daemonize
+
 
 # Replace target as you want.
-CHECK_TARGET = 'http://www.google.com'
+# We're using a static IP for Google to make sure our DNS doesn't fool us.
+CHECK_TARGET = 'http://74.125.224.72/' #'http://www.google.com'
 
 
 class SysTrayIcon(QSystemTrayIcon):
-    def __init__(self, parent=None):
-        super(SysTrayIcon, self).__init__(parent)
+  def __init__(self, parent=None):
+    super(SysTrayIcon, self).__init__(parent)
 
-        self.okIcon = QIcon(':/ok.png')
-        self.failIcon = QIcon(':/broken.png')
+    self.okIcon = QIcon(':/ok.png')
+    self.failIcon = QIcon(':/broken.png')
 
-        self.initUI()
+    self.initUI()
 
-        self.checkThread = CheckThread()
-        self.checkThread.start()
+    self.checkThread = CheckThread()
+    self.checkThread.start()
 
-        self.connect(self.checkThread, SIGNAL('inet'), self.statusToggle)
+    self.connect(self.checkThread, SIGNAL('inet'), self.statusToggle)
 
-    def initUI(self):
-        if self.isSystemTrayAvailable():
-            self.setIcon(self.okIcon)
+  def initUI(self):
+    if self.isSystemTrayAvailable():
+      self.setIcon(self.okIcon)
 
-            self.quitAction = QAction('Quit', self)
+      self.quitAction = QAction('Quit', self)
 
-            self.trayMenu = QMenu()
-            self.trayMenu.addAction(self.quitAction)
-            self.connect(self.quitAction, SIGNAL('triggered()'), self.quitAll)
+      self.trayMenu = QMenu()
+      self.trayMenu.addAction(self.quitAction)
+      self.connect(self.quitAction, SIGNAL('triggered()'), self.quitAll)
 
-            self.setContextMenu(self.trayMenu)
-            self.setVisible(True)
+      self.setContextMenu(self.trayMenu)
+      self.setVisible(True)
 
-    def statusToggle(self, status):
-        if status == 'OK':
-            self.setIcon(self.okIcon)
-        elif status == 'FAILED':
-            self.setIcon(self.failIcon)
+  def statusToggle(self, status):
+    if status == 'OK':
+      self.setIcon(self.okIcon)
+    elif status == 'FAILED':
+      self.setIcon(self.failIcon)
 
-    def quitAll(self):
-        self.checkThread.quit()
-        self.hide()
-        del self
-        sys.exit(0)
+  def quitAll(self):
+    self.checkThread.quit()
+    self.hide()
+    del self
+    sys.exit(0)
 
 
 class CheckThread(QThread):
-    def check_connection(self):
-        self.r = None
-        try:
-            self.r = requests.get(CHECK_TARGET, timeout=2.5)
-            if self.r.status_code == 200:
-                return 1
-            else:
-                return 0
-        except:
-            return 0
+  def check_connection(self):
+    self.r = None
+    try:
+      self.r = requests.get(CHECK_TARGET, timeout=2.5)
+      if self.r.status_code == 200:
+        return 1
+      else:
+        return 0
+    except:
+      return 0
 
-    def run(self):
-        while 1:
-            sleep(2)
+  def run(self):
+    while 1:
+      sleep(2)
 
-            # Trigger the checking.
-            ret = self.check_connection()
-            if ret:
-                self.emit(SIGNAL('inet'), 'OK')
-            else:
-                self.emit(SIGNAL('inet'), 'FAILED')
+      # Trigger the checking.
+      ret = self.check_connection()
+      if ret:
+        self.emit(SIGNAL('inet'), 'OK')
+      else:
+        self.emit(SIGNAL('inet'), 'FAILED')
 
-            # Clear the cache.
-            if self.r:
-                del self.r
+      # Clear the cache.
+      if self.r:
+        del self.r
 
+
+# ************************************************
+# non object orientated entry code goes down here:
+# ************************************************
+def start_internet_works():
+  app = QApplication([])
+  tray = SysTrayIcon(app)
+  tray.show()
+  sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    app = QApplication([])
-    tray = SysTrayIcon(app)
-    tray.show()
-    sys.exit(app.exec_())
+  parser = argparse.ArgumentParser(
+    description = 'A system tray icon indicates \
+      the Internet online & offline status'
+  )
+  parser.add_argument(
+    '-d',
+    '--daemon',
+    action = 'store_true',
+    dest = 'daemon',
+    help = 'enables daemon mode'
+  )
+  args = parser.parse_args()
+
+  if args.daemon:
+    daemon = Daemonize(
+      app='SleepServer Daemon',
+      pid='/tmp/sleepServerDaemon.pid',
+      action=start_internet_works
+    )
+    daemon.start()
+  else:
+    start_internet_works()
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-InternetWorks
-=============
+InternetWorks (Python 2 / OS X)
+===============================
 
-A system tray icon indicates the Internet online &amp; offline status.
+A system tray icon indicates the Internet online &amp; offline status
+for Mac OS X.
+
+This script starts a GET request to `http://74.125.224.72/` every 2.5 seconds. It is using a static IP address of Google to make sure a DNS server doesn't fool you about you internet connection.
+
+## Get it running
+
+### Requirements
+
+This script requires three non standard packages. You can install them using
+*pip* and *brew*:
+
+- Python Qt4 (`brew install pyqt`)
+- requests (`pip install requests`)
+- argparse (`pip install argparse`)
+- daemonize (`pip install daemonize`)
+
+### Start it
+
+To start the application, simply call:
+
+`python InternetWorks.py`
+
+It also comes with a daemon mode to run it as a background service:
+
+`python InternetWorks.py -d`
+
+***
+
+
+## Contribution & Contributors
+
+I'd love to see your ideas for improving this project!
+The best way to contribute is by submitting a pull request or a new Github issue. :octocat:
+
+## Apart of the code
+
+Thank you for reading this and for your interest in my work. I hope I could help you or even make your day a little better. Cheers!
